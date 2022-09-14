067f592a7b28f9ba1ebc7988c8cc6f63fe3af22a
jaezeu <jazeel_18@hotmail.com>
2022-09-14 20:08:11 +0800

    initial commit

---
+++ kubemaster.sh
@@ -0,0 +1,25 @@
+kubeadm init --config kubeadm-config.yaml --ignore-preflight-errors=all
+
+#NOTE THE OUTPUT WILL ALSO PRINT A WORKER NODE JOINING STRING which u have to take note, for the worker nodes to join the cluster
+#Once the control plane is initialized, run the following commands.
+
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+#NOTE THE OUTPUT WILL ALSO PRINT A WORKER NODE JOINING STRING which u have to take note, for the worker nodes to join the cluster
+
+
+#VERIFICATION:
+
+kubectl get pods -A
+
+#this command will show that all the pods are running besides the coredns pods which are stuck in pending state. This is because a network add on is yet to be installed.
+# https://kubernetes.io/docs/setup/production-environment/tools/kubeadm/troubleshooting-kubeadm/#coredns-is-stuck-in-the-pending-state
+# For this example, we can use weave as u can run a single command to install the network add ons
+
+kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
+
+#now try getting pods again to see the weave and coredns pods is in running state. And the control plane will be in ready state
+
+kubectl get pods -A
